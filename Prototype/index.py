754a88eda5da1d2ae1c5c243656a43c9febe6102
Mitchell Osborn <39538422+mitosborn@users.noreply.github.com>
2020-11-22 13:08:18 -0800

    Working on deployment issues

--- Prototype/index.py
+++ Prototype/index.py
@@ -79,4 +79,7 @@ def update_comp_chart(compare_mode):
 #     if sub_group == 'ECON':
 
 if __name__ == '__main__':
-    app.run_server(debug = True)
+    try:
+        app.run_server(debug=True)
+    except Exception as e:
+        print(e)
