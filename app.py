d7f1ce1803df2e1e54c3d07a3883e0d036f23ec8
Melinda Ding <mjd4@rice.edu>
2021-02-08 11:07:15 -0600

    update browser icon and title

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import dash 
-import dash_bootstrap_components as dbc 
+import dash
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, suppress_callback_exceptions = True, external_stylesheets=[dbc.themes.LUX])
+app = dash.Dash(__name__, title="TX Covid-19 Enviro-Map",
+                suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
 server = app.server
-
-
